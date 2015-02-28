062b4768c160cd0d0ed75790926bdc70c436daff
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-02-28 16:58:13 +0500

    Add some interactivity

--- demo.jsx
+++ demo.jsx
@@ -6,18 +6,22 @@ var YButton = require('./index.jsx!');
 
 React.render((
     <div>
-        <YButton>
-            Default
+        <YButton disabled>
+            Disabled
         </YButton>
+        <br/><br/>
         <YButton size="xs">
             XS size
         </YButton>
+        <br/><br/>
         <YButton size="s">
             S size
         </YButton>
+        <br/><br/>
         <YButton size="m">
             M size
         </YButton>
+        <br/><br/>
         <YButton size="l">
             L size
         </YButton>
--- index.jsx
+++ index.jsx
@@ -4,15 +4,46 @@ var b = require('b');
 var Style = require('./index.css!');
 
 var YButton = {
+	getInitialState: function() {
+        return {
+            hovered: false,
+			pressed: false
+        };
+    },
+
+	__onMouseEnter: function() {
+        this.setState({hovered: true});
+    },
+
+    __onMouseLeave: function() {
+        this.setState({hovered: false});
+    },
+
+	__onMouseDown: function() {
+        this.setState({pressed: true});
+    },
+
+	__onMouseUp: function() {
+        this.setState({pressed: false});
+    },
+
 	render: function () {
 		var classes = b('y-button', {
 			theme: this.props.theme || 'normal',
 			type: this.props.type,
-			size: this.props.size
+			size: this.props.size,
+			disabled: this.props.disabled,
+			hovered: this.state.hovered,
+			pressed: this.state.pressed
 		});
 
 		return (
-			<button className={classes}>
+			<button
+				className={classes}
+				onMouseEnter={this.__onMouseEnter}
+                onMouseLeave={this.__onMouseLeave}
+				onMouseDown={this.__onMouseDown}
+                onMouseUp={this.__onMouseUp}>
 				<span className={b('y-button', 'text')}>
 					{this.props.children}
 				</span>
